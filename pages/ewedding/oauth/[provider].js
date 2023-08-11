0923bcc5979160dd9ff14d02ede674cd5ee28db3
NoPaperDotCom <130741956+NoPaperDotCom@users.noreply.github.com>
2023-08-11 16:52:19 +0800

    Update [provider].js

--- pages/ewedding/oauth/[provider].js
+++ pages/ewedding/oauth/[provider].js
@@ -105,7 +105,7 @@ export async function getServerSideProps({ params, query, req, res }) {
   if (!_ret.user || !_ret.user.sessionToken) {
     return {
       redirect: {
-        destination: "/wedding",
+        destination: "/ewedding",
         permanent: false
       }
     };
