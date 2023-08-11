f8e2d77f4695e96a73ff5dd54929a257c299107f
NoPaperDotCom <130741956+NoPaperDotCom@users.noreply.github.com>
2023-08-11 16:49:36 +0800

    Update website.config.js

--- website.config.js
+++ website.config.js
@@ -2,5 +2,6 @@ module.exports = {
   LOCALSTORAGE_SESSION_TOKEN_KEY: "showmeyourlove:session-token",
   OAUTH_URL: "/ewedding/oauth",
   GOOGLE_OAUTH_URL: "/ewedding/oauth/google",
-  STRIPE_CHECKOUT_URL: "/ewedding/checkout/stripe"
+  STRIPE_CHECKOUT_URL: "/ewedding/checkout/stripe",
+  GOOGLE_GTAG_ID: "G-J6DJQYM1XS"
 };
