f024e5cd93f8ed8f40399753a97694e7fcc74897
NoPaperDotCom <130741956+NoPaperDotCom@users.noreply.github.com>
2023-09-01 15:46:38 +0800

    Delete utils directory

--- utils/error.js
+++
@@ -1,7 +0,0 @@
-class AppError extends Error {
-  constructor ({ text = "internal", status = 500, message = false }) {
-    super(`${text}_${status}${(!message) ? "" : "_"}${message}`);
-  }
-}
-
-export default AppError
