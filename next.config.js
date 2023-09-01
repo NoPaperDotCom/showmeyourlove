245f4d12b7dfb96aa4c36b95996ea60b5d19a52e
NoPaperDotCom <130741956+NoPaperDotCom@users.noreply.github.com>
2023-09-01 15:49:35 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,15 @@
-/** @type {import('next').NextConfig} */
-// const keys = require("./private.key.js");
-const websiteSetting = require("./website.config.js");
-
 const nextConfig = {
   // reactStrictMode: true,
   env: {
     ENABLE_STYLE_LOG: false,
     ENABLE_EVENT_LOG: true,
-    SHOWMEYOURLOVE_CONTACT: "85254055826",
+    GOOGLE_GTAG_ID: "G-J6DJQYM1XS"
+    NOPAPER_SESSION_TOKEN: "nopaper:session-token",
+    NOPAPER_URL: "https://nopaper.vercel.app/",
+    NOPAPER_SERVICE_NAME: "showmeyourlove",
     PARSE_SERVER_URL: "https://parseapi.back4app.com",
-    PARSE_APPLICATION_ID: "27bPUjVJ86FA0Wac8uZQou0XXFoLyufKEZBxCav2",
-    PARSE_REST_API_KEY: "OwJrknH09LV4tktLEAtFZvOiNotwUKuaa0WTWMR6",
-    // ...keys,
-    ...websiteSetting
+    PARSE_APPLICATION_ID: "2LOIOWJoTAauKeHsLD8fUhiX9Jzz8f9AvPzLhQ42",
+    PARSE_REST_API_KEY: "jge4934bicbxpLxHE0tlFFhvcJHDl85qZHcBzFtw"
   }
 };
 
